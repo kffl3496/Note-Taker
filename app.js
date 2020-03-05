5c7775de7f1cf2590a4c65743daabb44c9bbbb09
Alex <alexstiles126@gmail.com>
2020-03-04 23:23:31 -0500

    Created a versatile note-taker application that records the users input into notes on a dashboard they can use for reference.

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.get('/notes', function(res, res) {
+  res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
+
+app.get('/api/notes', function(req, res) {
+  fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
+    if(err) throw err;
+    const ret = JSON.parse(data);
+    res.send(ret);
+  });
+});
+
+app.post('/api/notes', function(req, res) {
+  fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
+    if(err) throw err;
+    let ret = JSON.parse(data);
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    ret.push(newNote);
+    fs.writeFile(path.join(__dirname + '/db/db.json'), JSON.stringify(ret), (err) => {
+      if (err) throw err;
+    });
+    res.send(ret);
+  });
+});
+
+app.delete('/api/notes/:id', function(req, res) {
+  fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
+    if(err) throw err;
+    const ret = JSON.parse(data);
+    const id = req.params.id;
+    const filteredArray = ret.filter(x => x.id !== id);
+    fs.writeFile(path.join(__dirname + '/db/db.json'), JSON.stringify(filteredArray), (err) => {
+      if (err) throw err;
+    });
+    res.send(ret);
+  });
+});
+
+app.listen(port);
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
